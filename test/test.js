fa287bfde47d90bad777d01af3414bd9b814e583
Bacra <bacra.woo@gmail.com>
2015-03-25 16:38:40 +0800

    Add: merge defaults config

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ assert.equal(config('LOG_PATH'), '/log/error/', 'read by step');
 
 assert.equal(mconfig('.log.conf.js', 'LOG_PATH2'), '/log/warn/', 'read directly');
 assert.equal(mconfig.default('LOG_PATH3'), '/log/nolog/', 'read by default method');
+assert.equal(config('LOG_PATH3'), '/log/nolog/', 'read default by merge method');
 
 assert.doesNotThrow(function() {
 	mconfig('no_this_confg.js');
@@ -17,6 +18,12 @@ assert.doesNotThrow(function() {
 assert.strictEqual(mconfig('no_this_config.js', 'param'), undefined, 'no value return undefined');
 assert.strictEqual(config('param'), undefined, 'no value return undefined2');
 
+
+// read all param
 assert.equal(mconfig('.log.conf.js', null).LOG_PATH, '/log/error/', 'read all config by null param');
 assert.equal(config(null).LOG_PATH, '/log/error/', 'read all config by null param2');
 
+// only method
+var onlyConfig = mconfig.only('.log.conf.js');
+assert.equal(onlyConfig('LOG_PATH'), '/log/error/', 'only config');
+assert.equal(onlyConfig('LOG_PATH3'), undefined, 'only config to read default');
